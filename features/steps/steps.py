a854a5d66d08c214abcca08cbe28b8045beeaeea
pawelus <pawelus@users.noreply.github.com>
2016-09-13 15:50:47 +0200

    Use payments page to fill in passenger data

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -1,11 +1,16 @@
 from behave import *
 from pages import HomePage
+from pages import PaymentPage
 import time
 
 use_step_matcher('re')
 
-@given(r'I make a booking from "(?P<from_airport>[A-Z]{3})" to "(?P<to_airport>[A-Z]{3})" on (?P<date>\d{2}/\d{2}/\d{4}) for (?P<adults>\d+) adults? and (?P<children>\d+) child(?P<plural>ren)?')
-def make_booking(context, from_airport, to_airport, date, adults, children, plural):
+@given(r'I make a booking from "(?P<from_airport>[A-Z]{3})"'
+        ' to "(?P<to_airport>[A-Z]{3})"'
+        ' on (?P<date>\d{2}/\d{2}/\d{4})'
+        ' for (?P<adults>\d+) adults?'
+        ' and (?P<children>\d+) child(?P<pl>ren)?')
+def make_booking(context, from_airport, to_airport, date, adults, children, pl):
     home_page = HomePage(context.browser)
     home_page.close_cookie_policy()
     home_page.select_one_way()
@@ -17,16 +22,21 @@ def make_booking(context, from_airport, to_airport, date, adults, children, plur
     if children > 0:
         search_page.close_promo_popup()
     search_page.choose_first_flight_regular_price()
-    #this is an ugly hack but for some reason I get "checkout took too-long" if I press continue too fast
+    #this is an ugly hack but for some reason I get "checkout took too long"
+    # if I press continue too fast
     time.sleep(1)
     extras_page = search_page.press_continue()
     extras_page.press_check_out()
     extras_page.close_seat_popup()
-    time.sleep(5)
 
-@when(r'I pay for booking with card details "(?P<number>\d{4}\s?\d{4}\s?\d{4}\s?\d{4})", "(?P<date>\d{2}/\d{2})" and "(?P<cvv>\d{3})"')
+@when(r'I pay for booking'
+        ' with card details "(?P<number>\d{4}\s?\d{4}\s?\d{4}\s?\d{4})"'
+        ', "(?P<date>\d{2}/\d{2})"'
+        ' and "(?P<cvv>\d{3})"')
 def pay_with_card(context, number, date, cvv):
-    pass
+    payment_page = PaymentPage(context.browser)
+    payment_page.fill_passenger_data()
+    time.sleep(5)
 
 @then('I should get payment declined message')
 def check_payment_declined(context):
