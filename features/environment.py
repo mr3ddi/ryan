e9261c6434dff6f20a7289453c858ebe1fbabaeb
pawelus <pawelus@users.noreply.github.com>
2016-09-13 14:33:43 +0200

    Added chromedriver binary to project to simplify setup

--- features/environment.py
+++ features/environment.py
@@ -1,3 +1,5 @@
+import sys
+import os
 from selenium import webdriver
 from pages import HomePage
 from selenium.webdriver.common.by import By
@@ -7,7 +9,9 @@ from selenium.webdriver.support import expected_conditions as EC
 def before_scenario(context, scenario):
     options = webdriver.ChromeOptions()
     options.add_argument("--kiosk")
-    context.browser = webdriver.Chrome(chrome_options=options)
+    chromedriver_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'chromedriver'))
+    print (chromedriver_path)
+    context.browser = webdriver.Chrome(executable_path=chromedriver_path, chrome_options=options)
     context.browser.implicitly_wait(10)
     context.browser.get(HomePage.address)
     #WebDriverWait(context.browser, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'div.col-departure-airport')))
